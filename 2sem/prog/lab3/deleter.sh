2be6431c31968c4e5cc1be2412fa77d85746f8ca
Cicada <hubbleadd@bk.ru>
2022-09-08 11:51:41 +0700

    Delete dats

--- 2sem/prog/lab3/deleter.sh
+++ 2sem/prog/lab3/deleter.sh
@@ -7,10 +7,10 @@ git commit -m "Delete dats"
 git rm exp2/*.txt
 git add .
 git commit -m "Delete txts"
-git rm exp2/./a.out
+git rm exp2/src/./a.out
 git add .
 git commit -m "Delete exe"
 
-git rm exp2/*.bit
+git rm exp2/src/*.bit
 git add .
 git commit -m "Delete units.bin"
