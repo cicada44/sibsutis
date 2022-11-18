aa4c64c60314e78e2f823221985cdbd6dc35024e
Cicada <cicada@44>
2022-11-18 17:06:03 +0700

    Simple moving auto

--- 3sem/OOP/cw/battle/src/main/main.cpp
+++ 3sem/OOP/cw/battle/src/main/main.cpp
@@ -2,6 +2,7 @@
 #include "../libfighters/Hp.hpp"
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <stdlib.h>
 #include <unistd.h>
 
 #define SHORT_DELAY 10
@@ -12,9 +13,105 @@
 using std::cin;
 using std::cout;
 
+int getrand(int min, int max) {
+  return (double)rand() / (RAND_MAX + 1.0) * (max - min) + min;
+}
+
 void gameover_display(sf::RenderWindow &window, int t);
 
 int main() {
+  // int f1_count_moves_x;
+  // int f1_count_moves_y;
+  // int f2_count_moves_x;
+  // int f2_count_moves_y;
+  // bool f1_shoot;
+  // bool f2_shoot;
+
+  Fighter f1(800, 600, sf::Color::Black, 1);
+  // Fighter f2(200, 600, sf::Color::Blue, 1);
+  Fighter_hp f2(200, 600, sf::Color::Black, 0);
+  // Fighter_ghost f2 = new Fighter(200, 600, sf::Color::Black, 1);
+
+  /* floor */
+  sf::Color floor_clr;
+  floor_clr.r = 105;
+  floor_clr.g = 105;
+  floor_clr.b = 105;
+  sf::RectangleShape floor;
+  floor.setSize(sf::Vector2f(1200, 190));
+  floor.setPosition(sf::Vector2f(0, 485));
+  floor.setFillColor(floor_clr);
+
+  /* background texture */
+  sf::Texture texture;
+  if (!texture.loadFromFile(
+          "/home/cicada44/cicada-main/sibsutis/3sem/OOP/cw/battle/"
+          "images/fr2.jpg")) {
+  }
+  sf::Sprite background(texture);
+
+  sf::RenderWindow window(sf::VideoMode(WINDOW_SIZE_X, WINDOW_SIZE_Y),
+                          "Fighter");
+
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed)
+        window.close();
+    }
+
+    f1.move_x(1);
+
+    // window.close();
+
+    // if (int i = getrand(1, 10)) {
+    //   if (i == 1) {
+    //     f1_count_moves_x = getrand(10, 40);
+    //     for (int x = 0; x < f1_count_moves_x; x++) {
+    //       f1.move_x(1);
+    //     }
+    //   }
+    //   if (i == 2) {
+    //     f1_count_moves_y = getrand(10, 40);
+    //     for (int x = 0; x < f1_count_moves_y; x++) {
+    //       f1.move_y(1);
+    //     }
+    //   }
+    //   if (i == 3) {
+    //     f1_shoot = getrand(0, 1);
+    //     if (f1_shoot)
+    //       f1.shoot(window, f2, floor);
+    //   }
+    // }
+
+    /* gameover */
+    if (static_cast<int>(f1.get_hp()) == 0) {
+      gameover_display(window, 0);
+    }
+    if (static_cast<int>(f2.get_hp()) == 0) {
+      gameover_display(window, 1);
+    }
+
+    /* clear window */
+    window.clear(sf::Color::White);
+
+    /* draw floor & hp*/
+    window.draw(floor);
+    window.draw(f1.get_hp_scale());
+    window.draw(f2.get_hp_scale());
+
+    f1.draw(window);
+    f2.draw(window);
+
+    window.display();
+
+    sf::sleep(sf::milliseconds(LONG_DELAY));
+  }
+
+  return 0;
+}
+
+void game() {
   Fighter f1(800, 600, sf::Color::Black, 1);
   // Fighter f2(200, 600, sf::Color::Blue, 1);
   Fighter_hp f2(200, 600, sf::Color::Black, 0);
@@ -72,12 +169,10 @@ int main() {
       }
     }
 
-    // cout << "hpleft - " << hp_left.get_hp() << '\n';
     if (static_cast<int>(f1.get_hp()) == 0) {
       gameover_display(window, 0);
     }
     if (static_cast<int>(f2.get_hp()) == 0) {
-
       gameover_display(window, 1);
     }
 
@@ -94,8 +189,6 @@ int main() {
 
     window.display();
   }
-
-  return 0;
 }
 
 void gameover_display(sf::RenderWindow &window, int t) {
@@ -108,7 +201,7 @@ void gameover_display(sf::RenderWindow &window, int t) {
   else if (t == 1)
     text.setString("PLAYER 2 WON");
   text.setFont(font);
-  text.setPosition(WINDOW_SIZE_X / 3, WINDOW_SIZE_Y / 3);
+  text.setPosition(450, WINDOW_SIZE_Y / 2);
   text.setCharacterSize(100);
   text.setCharacterSize(40);
   text.setFillColor(sf::Color::Green);
