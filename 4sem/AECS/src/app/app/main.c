5de083823c446b3bbfa1cf88a9b9bbdf5d8f6364
Cicada <hubbleadd@bk.ru>
2023-02-02 19:48:22 +0700

    Add all functions

--- 4sem/AECS/src/app/app/main.c
+++ 4sem/AECS/src/app/app/main.c
@@ -7,12 +7,46 @@
 void test()
 {
     printf("\t\t----- SIMPLE COMPUTER -----\n");
-    printf("\tCOMMANDS\nINIT MEMORY - 1\nSET VALUE - 2\nGET VALUE - 3\n");
+    printf(
+        "%-20s"
+        " - 1\n"
+        "SET VALUE"
+        " - 2\n"
+        "GET VALUE"
+        " - 3\n"
+        "SAVE "
+        "- 4\n"
+        "LOAD "
+        "- 5\n"
+        "REGISTER INIT"
+        " - 6\n"
+        "REGISTER SET "
+        "- 7\n"
+        "REGISTER GET "
+        "- 8\n",
+        "MEMORY INIT");
 }
 
 int main()
 {
-    test();
+    // test();
+
+    sc_memoryInit();
+
+    sc_memoryLoad("/home/cicada44/cicada-main/sibsutis/4sem/AECS/test_files_memory/mem1");
+
+    int *value = malloc(sizeof(int));
+
+    sc_commandEncode(0x33, 0x59, value);
+
+    // printf("%x\n", *value);
+
+    int *command = malloc(sizeof(int));
+    int *operand = malloc(sizeof(int));
+
+    sc_commandDecode(*value, command, operand);
+
+    // printf("%x\n", 0xffff);
 
     return 0;
 }
--- 4sem/AECS/src/libcomputer/libcomputer/comp.c
+++ 4sem/AECS/src/libcomputer/libcomputer/comp.c
@@ -4,13 +4,54 @@
 
 #include <stdio.h>
 
+#define ZERO_BIT 0x0
 #define ONE_BIT 0x1
+#define MASK_DECODE_COMMAND 0x7f
 
 static int flags;
 
 static int *memory = NULL;
 
-static int size_memory = 0;
+static int commands[] = {
+    10,
+    11,
+    20,
+    21,
+    30,
+    31,
+    32,
+    33,
+    40,
+    41,
+    42,
+    43,
+    51,
+    52,
+    53,
+    54,
+    55,
+    56,
+    57,
+    58,
+    59,
+    60,
+    61,
+    62,
+    63,
+    64,
+    65,
+    66,
+    67,
+    68,
+    69,
+    70,
+    71,
+    72,
+    73,
+    74,
+    75,
+    76,
+};
 
 int sc_memoryInit()
 {
@@ -18,7 +59,6 @@ int sc_memoryInit()
 
     if (memory == NULL)
     {
-        sc_regSet(1, 1);
         return -1;
     }
 
@@ -29,7 +69,7 @@ int sc_memorySet(int address, int value)
 {
     if (address < 0 || address > 99)
     {
-        sc_regSet(1, 1);
+        sc_regSet(3, 1);
         return -1;
     }
 
@@ -42,7 +82,7 @@ int sc_memoryGet(int address, int *value)
 {
     if (value == NULL || address < 0 || address > 99)
     {
-        sc_regSet(1, 1);
+        sc_regSet(3, 1);
         return -1;
     }
 
@@ -98,7 +138,7 @@ int sc_regInit()
 
 int sc_regSet(int reg, int value)
 {
-    if (reg < 0 || reg > 32 || (value != 0 && value != 1))
+    if (reg < 1 || reg > 5 || (value != 0 && value != 1))
     {
         return -1;
     }
@@ -110,12 +150,44 @@ int sc_regSet(int reg, int value)
 
 int sc_regGet(int reg, int *value)
 {
-    if (reg < 0 || reg > 32 || value == NULL)
+    if (reg < 1 || reg > 5 || value == NULL)
     {
         return -1;
     }
 
     *value = (flags >> (reg - 1)) & ONE_BIT;
+
+    return 0;
+}
+
+int sc_commandEncode(int command, int operand, int *value)
+{
+    if (bsearch(&command, commands, 38, sizeof(int), comp) == NULL || operand > 0x7f || value == NULL)
+    {
+        return -1;
+    }
+
+    *value = ZERO_BIT;
+
+    *value = command << 7;
+    *value |= operand;
+
+    return 0;
+}
+
+int sc_commandDecode(int value, int *command, int *operand)
+{
+    if (command == NULL || operand == NULL)
+    {
+        sc_regSet(5, 1);
+        return -1;
+    }
+
+    *operand = *command = ZERO_BIT;
+
+    *operand = value & MASK_DECODE_COMMAND;
+    value >>= 7;
+    *command = value;
 
     return 0;
 }
@@ -132,4 +204,16 @@ void output_memory()
     }
 
     printf("\n");
+}
+
+void bin(unsigned n)
+{
+    unsigned i;
+    for (i = 1 << 31; i > 0; i = i / 2)
+        (n & i) ? printf("1") : printf("0");
+}
+
+int comp(const void *n1, const void *n2)
+{
+    return (n1 > n2) ? -1 : 1;
 }
